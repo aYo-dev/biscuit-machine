700471185dd907c5b073b2ed495e4af6a9e0e380
Asen Yovchev <ayovcheff@gmail.com>
2022-09-30 17:18:35 +0300

    update biscuit machine logic

--- src/components/BiscuitMachine.tsx
+++ src/components/BiscuitMachine.tsx
@@ -2,18 +2,18 @@ import { Box, Button, Chip } from "@mui/material";
 import { Stack } from "@mui/system";
 import { cond, equals, isEmpty } from "ramda";
 import { useEffect, useState } from "react";
+import { TryCatch, Either } from 'lambda-ts';
+import ThermostatIcon from '@mui/icons-material/DeviceThermostat';
 
 import { BiscuitStates, MachineStates } from "../enums";
 import { useMotor } from "../hooks/use-motor";
 import { IBiscuit } from "../interfaces";
 import { BiscuitList } from "./BiscuitList";
-import ThermostatIcon from '@mui/icons-material/DeviceThermostat';
-import { TryCatch, Either } from 'lambda-ts';
 
 
 const isOn = equals(MachineStates.on);
 const isOff = equals(MachineStates.off);
-const isPause = equals(MachineStates.off);
+const isPause = equals(MachineStates.pause);
 
 interface BiscuitMachineProps{
   brand: string,
@@ -22,15 +22,16 @@ interface BiscuitMachineProps{
 export const BiscuitMachine = (props: BiscuitMachineProps) => {
   const [machineState, switchMachine] = useState(MachineStates.off);
   const [temperature, setTemperature] = useState(0);
-  const [setMotorState, pulse] = useMotor();
+  const {pulse, motorState, setMotorState} = useMotor();
 
   const [biscuitsForConvey, addBiscuitsForConvey] = useState([] as IBiscuit[]);
+  // when cookies are ready they should be stored in the basket
   const [basket, addToBasket] = useState([] as IBiscuit[]);
   const [raw, addRaw] = useState({} as IBiscuit);
   const [stamped, addStamped] = useState({} as IBiscuit);
   const [inOven, putInOven] = useState({} as IBiscuit);
 
-  const extrude = (id: number) => ({id, stamp: '', state: BiscuitStates.raw});
+  const extrude = (id: string) => ({id, stamp: '', state: BiscuitStates.raw});
   const stamp = (biscuite: IBiscuit) => ({...biscuite, stamp: props.brand, state: BiscuitStates.stamped});
   const bake = (biscuite: IBiscuit) => ({...biscuite, state: BiscuitStates.bake});
   const done = (biscuite: IBiscuit) => ({...biscuite, state: BiscuitStates.baked});
@@ -42,21 +43,16 @@ export const BiscuitMachine = (props: BiscuitMachineProps) => {
 
   const stop = () => {
     switchMachine(MachineStates.off);
-    setMotorState(false);
   }
 
   const pause = () => {
-    switchMachine(MachineStates.pause)
+    switchMachine(MachineStates.pause);
     setMotorState(false);
   }
 
   useEffect(() => {
     if(isOn(machineState)) {
       setTemperature(240);
-    }
-
-    if(isOff(machineState)) {
-      setTemperature(0);
     }
   }, [machineState]);
 
@@ -76,38 +72,81 @@ export const BiscuitMachine = (props: BiscuitMachineProps) => {
       .map(el => [...el, ...getBiscuitSafer(stamped)])
       .map(el => [...el, ...getBiscuitSafer(inOven)])
       .get();
-
     // update convey belt
     addBiscuitsForConvey(updatedList);
   }
 
-  useEffect(() => {
-    if(!isOn(machineState)) return;
-    // The order is important 
-    // first if there is any stamped cookies it should be put in the oven 
-    // to free a space for the new stamped one
-    // then we should check if there is any raw cookie and if so it should be stamped
-    // and only then when we are sure that there is a free space in the convaey belt 
-    // we can create new raw cookie with the extruder
-
-    // if there is in the oven we should put them out because it will burn  
-    !isEmpty(inOven) && addToBasket((prev) => [...prev, done(inOven)]);
-    // if there is stamped cookie we should put in the oven
-    !isEmpty(stamped) && putInOven(bake(stamped)) 
-    // if there is raw cookie we should stamp it
+  // when machine is on convey belt should be start the process of creation of the cookises
+  const handlePulseWhenMachineIsTurnedOn = () => {
+    // machine create raw cookie with the extruder
+    addRaw(extrude(new Date().toLocaleString()));
+    // if there is a raw cookie it must be stamped
     !isEmpty(raw) && addStamped(stamp(raw));
-    // The id is the pulse on this way we could track the process
-    addRaw(extrude(pulse));
+    // if there is a stamped raw cookie it must be put in the oven
+    !isEmpty(stamped) && putInOven(bake(stamped));
+    // if there is a baked cookie it should be store in the basket
+    !isEmpty(inOven) && addToBasket((prev) => [done(inOven), ...prev]);
+  }
 
-    updateConveyorBelt();
+  // when machine is turned off convey belt must be emptied 
+  // and all cookies on the belt must be baked
+  const handlePulseWhenMachineIsTurnedOff = () => {
+    // if there is cookie in the oven it must be moved in the basked 
+    // and oven should be turn off
+    if(!isEmpty(inOven)) {
+      addToBasket((prev) => [done(inOven), ...prev]);
+      putInOven({} as IBiscuit);
+      setTemperature(0);
+    }
+
+    // if there is stmped cookie it must be put in oven and 
+    // then we don't need any new stemped cookie
+    if (!isEmpty(stamped)) {
+      putInOven(bake(stamped));
+      addStamped({} as IBiscuit);
+    }
+
+    // if there is raw cookie it must be stamped and 
+    // then we don't need any new raw cookie
+    if (!isEmpty(raw)) {
+      addStamped(stamp(raw));
+      addRaw({} as IBiscuit);
+    }
+  }
+
+  useEffect(() => {
+    if(!motorState) return; 
+    
+    if(isOn(machineState)) {
+      handlePulseWhenMachineIsTurnedOn();
+    }
+
+    if(isOff(machineState)) {
+      handlePulseWhenMachineIsTurnedOff();
+    }
+
   }, [pulse]);
+
+  useEffect(() => {
+    updateConveyorBelt();
+  }, [raw, stamped, inOven]);
+
+  useEffect(() => {
+    if(!isOff(machineState)) return;
+    if(!isEmpty(biscuitsForConvey)) return;
+
+    // motor should stop only when conveyor belt is empty
+    setMotorState(false);
+  }, [biscuitsForConvey, machineState]);
+
+
 
   return (<>
     <Box display="flex" alignItems='center' flexDirection="column">
       <Stack direction="row" spacing={2} padding={2}>
-        <Button variant="contained" onClick={start}>On</Button>
-        <Button variant="contained" disabled={isOff(machineState)} onClick={pause}>Pause</Button>
-        <Button variant="contained" onClick={stop}>Off</Button>
+        <Button variant="contained" onClick={start} disabled={isOn(machineState)}>On</Button>
+        <Button variant="contained" disabled={isOff(machineState) || isPause(machineState)} onClick={pause}>Pause</Button>
+        <Button variant="contained" disabled={isOff(machineState) || isPause(machineState)} onClick={stop}>Off</Button>
         <Chip icon={<ThermostatIcon />} label={temperature} color={getTermostatColor(temperature) as any}/>
       </Stack>
       {!biscuitsForConvey && <p>It looks like the belt is empty...</p>}

2732178871a3689c1f5e5ef14bf39884d961164c
Asen Yovchev <ayovcheff@gmail.com>
2022-10-01 15:13:43 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,19 @@ import { isEmpty } from 'ramda';
 
 function App() {
   const [brand, setBrand] = useState('');
-  const showAlert = useMemo(() => isEmpty(brand), [brand]);
+  const isBrandEmpty = useMemo(() => isEmpty(brand), [brand]);
 
   return (
     <div className="App">
       <Container maxWidth="md">
-        <Typography variant="h2" component="h1">Cookie factory</Typography>
+        <Typography variant="h2" component="h1">Cookie Factory</Typography>
         <TextField label="Brand" variant="outlined" onChange={(el) => setBrand(el.target.value)}/>
-        <Fade in={showAlert} timeout={1000}>
+        <Fade in={isBrandEmpty} timeout={1000}>
           <Box padding={3}>
             <Alert severity="info">Please enter a brand name to continue!</Alert>
           </Box>
         </Fade>
-        <BiscuitMachine brand={brand} />
+        <BiscuitMachine brand={brand} canStart={!isBrandEmpty}/>
       </Container>
     </div>
   );
